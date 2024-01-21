3c4dadd473ca961a62579cd7341bab9e427cb3ca
guyllaumedemers <guyllaume_demers@hotmail.fr>
2024-01-21 13:42:39 -0500

    pushing initial COMMIT for build.bat as I will not be using CMake to provide compiler/linker flags so directories arent durtied by cmakelists

---
+++ Engine/Source/Runtime/Launch/Private/Launch.cpp
@@ -0,0 +1,83 @@
+//Copyright(c) 2024 gdemers
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+#include "../../Core/Public/CoreMinimal.h"
+#include "../../Core/Public/Misc/CoreGlobals.h"
+
+extern int32_t EnginePreInit() {
+
+	int32_t ErrorLevel = 0;
+	// load required library
+
+	return ErrorLevel;
+}
+
+extern int32_t EngineInit() {
+
+	int32_t ErrorLevel = 0;
+	// create a window application
+
+	return ErrorLevel;
+}
+
+extern void EngineTick() {
+
+	// tick engine
+
+}
+
+extern void EnginePreExit() {
+
+	// flush resources, subsystems, etc...
+
+}
+
+extern void EngineExit() {
+
+	// close libraries
+
+}
+
+// cross-platform entry point to engine startup
+int32_t GuardedMain() {
+
+	// handle library loading for engine
+	int32_t ErrorLevel = EnginePreInit();
+
+	// handle system initialization
+	ErrorLevel = EngineInit();
+
+	// TODO @gdemers 2024-01-09 Inspect Error level and react properly to value - Check level meaning online
+
+	// handle application loop
+	while (!IsEngineExitRequested()) {
+
+		// handle engine tick
+		EngineTick();
+	}
+
+	// handle flushing resources acquired at runtime
+	EnginePreExit();
+
+	// handle releasing library modules, dll
+	EngineExit();
+
+	return ErrorLevel;
+};
---
+++ Engine/Source/build.bat
@@ -0,0 +1,35 @@
+:: Build script for engine
+
+:: online resource - https://www.tutorialspoint.com/batch_script/batch_script_commands.htm
+
+:: turn off command display. '@' apply command to itself
+@ECHO OFF
+
+:: cmd-args can be provided through '%<n>', where <n> is the arg index and forwarded during invocation of the .bat file
+:: or through 'set /A variable-name=value'
+
+:: variable-name			- is the name of the variable you want to set.
+:: value					- is the value which needs to be set against the variable.
+:: /A						- is used if the value needs to be numeric in nature. *All of the Arithmetic operations work in batch*
+
+:: SETLOCAL / ENDLOCAL		- Starts localization of environment variables in a batch file
+:: online resouce			- https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/setlocal
+
+:: CALL						- Calls one batch program from another without stopping the parent batch program.			
+:: online resouce			- https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/call
+
+:: enabledelayeexpansion	- instructs cmd to recognise the syntax !var! which accesses the current value of var.
+:: online resouce			- https://stackoverflow.com/questions/22278456/enable-and-disable-delayed-expansion-what-does-it-do
+SETLOCAL enabledelayedexpansion
+
+:: get list of all *.cpp	- https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for
+SET cppFilenames=
+FOR /f usebackq %%i in (`DIR /ad /b %~dp0 ^| FINDSTR /v /i ThirdParty`) do (
+	:: update current directory as FOR /r [drive:path] doesnt support %VAR%
+	PUSHD "!dirRuntime!%%i\"
+	FOR /r %%k in (*.cpp) do (
+		SET cppFilenames=!cppFilenames! %%k
+		ECHO %%k
+	)
+	POPD
+)
